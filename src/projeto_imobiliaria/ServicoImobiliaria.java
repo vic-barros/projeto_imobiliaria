077c032bdcbdceebfe50bfd44b05ec97bb4700ef
vic-barros <victoria2425laissa@gmail.com>
2026-06-30 11:17:08 -0300

    Inicia Classe ServicoImobiliaria com método cadastrarCliente, falta ajustar a exceção dele

---
+++ src/projeto_imobiliaria/ServicoImobiliaria.java
@@ -0,0 +1,29 @@
+package projeto_imobiliaria;
+
+import java.util.ArrayList;
+
+public class ServicoImobiliaria {
+	private ArrayList<Cliente> clientes;
+	private ArrayList<Imovel> imoveis;
+	private ArrayList<Contrato> contratos;
+
+	public ServicoImobiliaria() {
+		this.clientes = new ArrayList<Cliente>();
+		this.imoveis = new ArrayList<Imovel>();
+		this.contratos = new ArrayList<Contrato>();
+	}
+
+	public void cadastrarCliente(Cliente c) {
+		try {
+			if(this.clientes.contains(c)) {
+				throw new ValidacaoException("Erro: Já existe um cleinte cadastrado com o CPF "  + c.getCpf());
+			}else {
+				clientes.add(c);
+				System.out.println("Cliente cadastrado com sucesso!");
+			}
+		}catch(ValidacaoException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+}
